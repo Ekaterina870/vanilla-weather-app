9add15ac92bf5733f594e9a1d75224e697a3484e
Ekaterina870 <122622907+Ekaterina870@users.noreply.github.com>
2023-02-16 22:36:37 +0000

    axios link added in html/app.js updated

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 let apiKey = "aa4349d1502cfb42ae79dd3817ceotf1";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+axios.get(url).then(showTemperature);
