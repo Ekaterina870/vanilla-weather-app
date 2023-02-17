b183c1150fadf254eb44afa48cec80f243c3045c
Ekaterina870 <122622907+Ekaterina870@users.noreply.github.com>
2023-02-17 12:18:00 +0000

    showTemperature function added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function showTemperature(response) {
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.temperature.feels_like
+  );
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
 let apiKey = "aa4349d1502cfb42ae79dd3817ceotf1";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
-axios.get(url).then(showTemperature);
+axios.get(apiURL).then(showTemperature);
